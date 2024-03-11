7bb2a20d8f01c860ba22dfb72cb4d97ca12f8ad8
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2024-03-11 11:50:34 -0700

    Update providers.tf
    Change to use TF from private registry

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,10 @@
 terraform {
   required_providers {
     rafay = {
-      version = "= 1.1.26"
-      source = "registry.terraform.io/RafaySystems/rafay"
+      #version = "= 1.1.26"
+      #source = "registry.terraform.io/RafaySystems/rafay"
+      version = "= 1.0.1"
+      source = "app.terraform.io/tfcd/rafay"
     }
   }
 }
